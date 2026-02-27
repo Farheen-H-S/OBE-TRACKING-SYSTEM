9c8078bfcc7a399a0dc2868319ba9613658a966f
Farheen-H-S <farheen040407@gmail.com>
2026-02-27 06:02:11 +0530

    CLeaned up local db

--- backend/notifications/migrations/0002_initial.py
+++ backend/notifications/migrations/0002_initial.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('bulk_upload', '0001_initial'),
+        ('notifications', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='bulkuploadjob',
-            name='uploaded_by',
-            field=models.ForeignKey(db_column='uploaded_by', on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL),
+            model_name='notification',
+            name='recipient',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='notifications', to=settings.AUTH_USER_MODEL),
         ),
     ]
