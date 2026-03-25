0c817c712527d83c09d982391ab8341f50023b9a
Farheen-H-S <farheen040407@gmail.com>
2026-03-25 19:32:34 +0530

    Final config done (hopefully)

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = []
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
