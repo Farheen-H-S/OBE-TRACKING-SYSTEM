30eafc4d3933f2323c7c1642baf1ad08737bb717
Farheen-H-S <farheen040407@gmail.com>
2026-04-04 00:54:44 +0530

    Updated catch all path to handle images too

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('api/academics/', include('academics.urls')),
     path('api/cis_master/', include('cis_master.urls')),
     path('api/assessments/', include('assessments.urls')),
-    # path('api/indirect_attainment/', include('indirect_attainment.urls')),
     path('api/attainment/', include('attainment.urls')),
     path('api/reports/', include('reports.urls')),
     path('api/stress/', include('stress.urls')),
@@ -21,9 +20,9 @@ urlpatterns = [
     path('api/bulk_upload/', include('bulk_upload.urls')),
     path('api/notifications/', include('notifications.urls')),
 
-    # ✅ FIXED catch-all
+    # ✅ FIXED catch-all (added images + manifest + favicon)
     re_path(
-        r'^(?!api/|static/|media/|images/|.*\.(png|jpg|jpeg|gif|svg|webp|ico|json)$).*$',
+        r'^(?!static/|media/|api/|images/|manifest\.json|favicon\.ico).*$',
         TemplateView.as_view(template_name='index.html')
     ),
 ]
