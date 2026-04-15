973487734f55f1ca4d8f29f023962dd22174757f
Farheen-H-S <farheen040407@gmail.com>
2026-04-15 11:27:46 +0530

    Fixing email error

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -161,10 +161,10 @@ if FRONTEND_URL:
 # =========================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_TIMEOUT = 10  # Seconds
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 20  # Seconds
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = f'OBE Tracking System <{EMAIL_HOST_USER}>'
